6df7443b0036bb5b2b9ae69688c201b97f57de20
Kelly Becker-Neuding <kbecker@kellybecker.me>
2016-08-14 15:39:40 -0700

    WIP Added Command Line Options
    Need to get the linking code from my laptop.
    I don't want to re-write it, I know lazy; am I right?

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ export COMPILERS_PATH=""
 export COMPILERS_REPO="https://github.com/KellyLSB/android-compilers.git"
 export COMPILERS_BRANCH=""
 export COMPILERS_GROUPS=()
+export COMPILERS_INSTALL2BIN=0
+export COMPILERS_LINK2CCACHE=0
 
 if [ $(id -u) -gt 0 ]; then
 	COMPILERS_PATH="${HOME}/.android-compilers"
@@ -11,7 +13,7 @@ else
 	COMPILERS_PATH="/opt/android-compilers"
 fi
 
-eval set -- "$(getopt -o p:g:r: --long path:,group:groups:,repo:,git: -- "$@")"
+eval set -- "$(getopt -o p:g:r:bc --long path:,group:groups:,repo:,git:,bin,ccache -- "$@")"
 
 while true; do
 	case "$1" in
@@ -19,6 +21,8 @@ while true; do
 	--repo|--git|-r) 	COMPILERS_REPO="$2"; 		shift 2;;
 	--branch|-b) 		COMPILERS_BRANCH="$2"; 		shift 2;;
 	--groups|--group|-g) 	COMPILERS_GROUPS+=("$2"); 	shift 2;;
+	--bin|-b)               COMPILERS_INSTALL2BIN=1;        shift 1;;
+	--ccache|-c)            COMPILERS_LINK2CCACHE=1;        shift 1;;
 	--) 			shift 1; 			break;;
 	esac
 done
